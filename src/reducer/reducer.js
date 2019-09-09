f0fbf2ab66db9c8a1cc8946df6f4c25da96844cb
Christopher Hernandez <cerberean661@gmail.com>
2019-09-09 05:04:22 -0700

    Add Todo button works

--- src/reducer/reducer.js
+++ src/reducer/reducer.js
@@ -7,7 +7,17 @@ export const initialState = {
 };
 
 export function reducer(state, action) {
+    // console.log('action', action);
     switch (action.type) {
+        case "ADD_TODO":
+            return{
+                ...state,
+                todoList: [...state.todoList, {
+                    item: action.payload,
+                    completed: false,
+                    id: Date.now()
+                }]
+            };
     default:
     return state;
     }
