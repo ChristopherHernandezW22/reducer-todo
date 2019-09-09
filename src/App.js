167439b0a68041004d7381da084c2b1a1def8a0f
Christopher Hernandez <cerberean661@gmail.com>
2019-09-09 04:56:31 -0700

    creating Form.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,20 @@ import './App.css';
 
 import { initialState, reducer } from './reducer/reducer';
 import Todo from './components/Todo';
+import Form from './components/Form';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log('state', state);
+
+  const addTodo = item => {
+    dispatch({ type: "ADD_TODO", payload: item});
+  };
+
   return (
     <div className="App">
       <Todo todoList={state.todoList}/>
+      <Form addTodo={addTodo} />
     </div>
   );
 }
---
+++ src/components/Form.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+const Form = ({ addTodo }) => {
+    const [item, setItem] = useState("");
+    const handleChange = event => setItem(event.target.value);
+    const handleSubmit = event => {
+        event.preventDefault();
+        addTodo(item);
+        setItem("");
+    };
+    return(
+        <form onSubmit={handleSubmit}>
+            <input type ="text" 
+                    name="item"
+                    placeholder="todo"
+                    value={item}
+                    onChange={handleChange}
+                    />
+            <button type="submit">Add Todo</button>
+        </form>
+    )
+};
+
+export default Form;
